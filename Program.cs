5885318e0e728856aa3ed0e35e39985779a027bb
DESKTOP-UJO5UUV\DELL <yachita.poohping@mail.kmutt.ac.th>
2021-04-24 15:47:32 +0700

    test6jaa

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace testmid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char input = char.Parse(Console.ReadLine());
+            int wide = int.Parse(Console.ReadLine());
+            int high = int.Parse(Console.ReadLine());
+            int number = int.Parse(Console.ReadLine());
+            int round = 1;
+            while (round <= number) 
+            {
+                for(int i = 1; i <= high; i++) 
+                {
+                    for (int j = 1; j <= round * wide; j++) 
+                    {
+                        Console.Write(input);
+                    }
+                    Console.WriteLine();
+                }
+                round++;
+                
+            }        
+            Console.ReadLine();
+
+        }
+    }
+}
